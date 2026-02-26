c9781f93c02f60fba49449e384877f3cf559fdae
dev <dev@local>
2026-02-26 10:05:13 -0600

    fixed sku names

--- src/lib/pricing/skuDefaults.ts
+++ src/lib/pricing/skuDefaults.ts
@@ -12,7 +12,6 @@
  */
 
 export const SKU_BOX_DURATION_MONTHS: Record<string, number> = {
-
   // =========================
   // VISTAKON
   // =========================
@@ -23,7 +22,7 @@ export const SKU_BOX_DURATION_MONTHS: Record<string, number> = {
   OASYS_MAX_AST_30: 1,
   OASYS_MAX_MF_30: 1,
   OASYS_MAX_MF_90: 3,
-  OASYS_MAX_MF_AST_30: 1,
+  OASYS_MAX_AST_MF_30: 1,
 
   // Acuvue Oasys 1-Day
   OASYS_1DAY_90: 3,
@@ -52,6 +51,9 @@ export const SKU_BOX_DURATION_MONTHS: Record<string, number> = {
   // Define
   DEFINE_30: 1,
 
+  //Acuvue 2
+  ACUVUE2_6: 3,
+
   // =========================
   // ALCON
   // =========================
@@ -68,7 +70,7 @@ export const SKU_BOX_DURATION_MONTHS: Record<string, number> = {
   TOTAL30_6: 6,
   TOTAL30_AST_6: 6,
   TOTAL30_MF_6: 6,
-  TOTAL30_MF_AST_6: 6,
+  TOTAL30_AST_MF_6: 6,
 
   // Precision1
   PRECISION1_30: 1,
@@ -106,6 +108,84 @@ export const SKU_BOX_DURATION_MONTHS: Record<string, number> = {
   // Bausch + Lomb
   // =========================
 
-  // ULTRA_6PK: 6,
-  // ULTRA_FOR_ASTIGMATISM_6PK: 6,
+  //Infuse
+  INFUSE_90: 3,
+  INFUSE_AST_90: 3,
+  INFUSE_MF_90: 3,
+
+  //Biotrue
+  BIOTRUE_ONEDAY_30: 1,
+  BIOTRUE_ONEDAY_90: 3,
+  BIOTRUE_ONEDAY_AST_30: 1,
+  BIOTRUE_ONEDAY_AST_90: 3,
+  BIOTRUE_ONEDAY_MF_30: 1,
+  BIOTRUE_ONEDAY_MF_90: 3,
+
+  //Ultra
+  ULTRA_6: 6,
+  ULTRA_AST_6: 6,
+  ULTRA_MF_6: 6,
+  ULTRA_MF_AST_6: 6,
+
+  //PureVision
+  PUREVISION_6: 6,
+  PUREVISION_MF_6: 6,
+  PUREVISION2_6: 6,
+  PUREVISION2_AST_6: 6,
+  PUREVISION2_MF_6: 6,
+
+  //SofLens
+  SOFLENS_DAILY_DISPOSABLE_90: 3,
+  SOFLENS_6: 3,
+  SOFLENS_AST_6: 3,
+  SOFLENS_MF_6: 3,
+
+
+// =========================
+// CooperVision
+// =========================
+
+  //clariti
+  CLARITI_1DAY_30: 1,
+  CLARITI_1DAY_90: 3,
+  CLARITI_1DAY_MF_30: 1,
+  CLARITI_1DAY_MF_90: 3,
+  CLARITI_1DAY_AST_30: 1,
+  CLARITI_1DAY_AST_90: 3,
+
+  //MyDay
+  MYDAY_90: 3,
+  MYDAY_180: 6,
+  MYDAY_ENERGYS_90: 3,
+  MYDAY_AST_90: 3,
+  MYDAY_MF_90: 3,
+
+  //Proclear
+  PROCLEAR_1DAY_90: 3,
+  PROCLEAR_1DAY_MF_90: 3,
+  PROCLEAR_6: 6,
+  PROCLEAR_AST_6: 6,
+  PROCLEAR_XR_AST_6: 6,
+  PROCLEAR_AST_MF_6: 3,
+  PROCLEAR_MF_6: 6,
+  PROCLEAR_XR_MF_6: 6,
+
+  //Avaira
+  AVAIRA_VITALITY_6: 3,
+  AVAIRA_VITALITY_AST_6: 3,
+
+  //Biofinity
+  BIOFINITY_6: 6,
+  BIOFINITY_XR_6: 6,
+  BIOFINITY_ENERGYS_6: 6,
+  BIOFINITY_MF_6: 6,
+  BIOFINITY_AST_6: 6,
+  BIOFINITY_XR_AST_6: 6,
+  BIOFINITY_AST_MF_6: 6,
+
+  //Biomedics
+  BIOMEDICS_55_6: 3,
+  BIOMEDICS_AST_6: 3,
+
+
 };
